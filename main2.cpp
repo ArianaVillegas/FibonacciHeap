ec298fd6f555899f6136902285e14a7a0668884e
cesar <camgcamg11@gmail.com>
2020-10-21 17:13:02 -0500

    allroutes

--- main2.cpp
+++ main2.cpp
@@ -1,12 +1,12 @@
-#include <glob.h>
 #include <vector>
 #include <iostream>
 #include <string.h>
 #include "CImg.h"
+#include <fstream>
 
 using namespace std;
-
-
+string temp;
+vector<string> files;
 using namespace cimg_library;
 
 
@@ -20,39 +20,32 @@ vector<float>  Vectorizar(CImg<float>   & img )
          return R;
 }
 
-
-vector<string> globVector(const string& pattern){
-    glob_t glob_result;
-    glob(pattern.c_str(),GLOB_TILDE,NULL,&glob_result);
-    vector<string> files;
-    for(unsigned int i=0;i<glob_result.gl_pathc;++i){
-        files.push_back(string(glob_result.gl_pathv[i]));
-    }
-    globfree(&glob_result);
-    return files;
-}
-
 int main()
 {
-    vector<string> files = globVector("./*/");
-    for(auto it:files){
-        cout<<it<<endl;
-    }
-   
+    //find /home/cesar/Desktop/git/eda/nuevo\ inicio\ /FibonacciHeap -type f -exec file --mime-type {}  \; | awk '{if ($NF == "image/jpeg") print $3 }' >files.txt
+    fstream a("files.txt");
     
-   CImg<float>   A("cara.jpeg");
-   CImg<float>   B =  A.haar(false,3);
-   CImg<float>   c  = B.crop(0,0,27,27);
+    while(a>>temp){
+        temp.pop_back();
+        files.push_back(temp);
+    }  
+    for(auto it:files){
+        CImg<float>   A(it.c_str());
+        CImg<float>   B =  A.haar(false,3);
+        CImg<float>   c  = B.crop(0,0,27,27);
+        vector<float> vc = Vectorizar(c);
 
-   vector<float> vc = Vectorizar(c);
+        cout<<it<<" ";
+        for ( auto it2:vc){
+            cout<<it2<<" ";
+        }
+        cout<<endl;
+    }
 
-   A.display();
-   B.display();
-   c.display();
 
    
    
-   return 1;
+    return 1;
 }
 //-lX11 y -pthread
 
