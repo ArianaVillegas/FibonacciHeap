2fd64e748a06c7debbbc5aef7afee9c37d5d1957
ArianaVillegas <ariana.villegas@utec.edu.pe>
2020-10-23 01:04:41 -0500

    Read data

--- Graph.h
+++ Graph.h
@@ -11,13 +11,12 @@ class Graph{
 
 	vector<Edge> edges;
 	vector<int> parent;
-	vector<int> rank;
+	vector<string> image_names;
 
 	int size = 0;
 
 	void make_set(int v) {
 	    parent[v] = v;
-	    rank[v] = 0;
 	}
 
 	int find_set(int v) {
@@ -30,11 +29,7 @@ class Graph{
 	    a = find_set(a);
 	    b = find_set(b);
 	    if (a != b) {
-	        if (rank[a] < rank[b])
-	            swap(a, b);
 	        parent[b] = a;
-	        if (rank[a] == rank[b])
-	            rank[a]++;
 	    }
 	}
 
@@ -50,8 +45,14 @@ public:
 		this->size = size;
 	}
 
+	void setImageNames(vector<string> image_names){
+		this->image_names = image_names;
+	}
+
 	Graph<T> kruskal(){
 		Graph<T> temp;
+		temp.setSize(size);
+		temp.setImageNames(image_names);
 		FibonacciHeap<T> variable;
 
 		for(Edge e:edges){
@@ -59,9 +60,7 @@ public:
 		}
 
 		parent.clear();
-		rank.clear();
 		parent.resize(size);
-		rank.resize(size);
 
 		for(int i=0; i<size; i++){
 			this->make_set(i);
@@ -71,6 +70,7 @@ public:
 			NodeB<T>* min = variable.extractMin();
 			if(find_set(min->from) != find_set(min->to)){
 				temp.insert(min->from, min->to, min->key);
+				cout << min->from << ' ' << min->to << '\n';
 				union_sets(min->from, min->to);
 			}
 		}
@@ -88,6 +88,9 @@ public:
         fstream output("graphviz.dot", ios::out | ios::trunc);
         output << "graph " << endl;
         output << "{" << endl;
+        for(int i=0; i<size; i++){
+        	output << i << "[image=\"" << image_names[i] << "\",label=\"\"];" << endl;
+        }
         for(Edge e : this->edges){
             output << e.from << " -- " << e.to << "[label=\"" << e.value << "\",";
             output << "weight=\"" << e.value << "\"];" << endl;
